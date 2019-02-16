5f0248d941a17b683c3f189fc4dbf6728f7bd8d8
dot-bob <robfrommendon@gmail.com>
2019-02-15 21:09:53 -0700

    Increase stepper currents.

--- Firmware/Configuration_prusa.h
+++ Firmware/Configuration_prusa.h
@@ -255,8 +255,8 @@
 #define TMC2130_SG_THRS_E       3     // stallguard sensitivity for E axis
 
 //new settings is possible for vsense = 1, running current value > 31 set vsense to zero and shift both currents by 1 bit right (Z axis only)
-#define TMC2130_CURRENTS_H {16, 20, 35, 30}  // default holding currents for all axes
-#define TMC2130_CURRENTS_R {16, 20, 35, 30}  // default running currents for all axes
+#define TMC2130_CURRENTS_H {35, 40, 35, 30}  // default holding currents for all axes
+#define TMC2130_CURRENTS_R {35, 40, 35, 30}  // default running currents for all axes
 #define TMC2130_UNLOAD_CURRENT_R 12			 // lowe current for M600 to protect filament sensor 
 
 #define TMC2130_STEALTH_Z
